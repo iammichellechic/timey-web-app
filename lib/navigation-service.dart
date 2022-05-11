517957ed597bc05e14be0dd368da3b6bace4837a
Christian Ekenstedt <christian.ekenstedt@zero8.io>
2022-05-11 21:52:07 +0200

    Few fixes

--- lib/navigation-service.dart
+++ lib/navigation-service.dart
@@ -1,8 +1,14 @@
+import 'dart:js';
+
 import 'package:flutter/widgets.dart';
 
 class NavigationService {
   final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
   Future<dynamic> navigateTo(String routeName) {
+    var curr = ModalRoute.of(navigatorKey.currentContext!);
+    if (curr != null) {
+      print(ModalRoute.of(navigatorKey.currentContext!)!.settings.name);
+    }
     return navigatorKey.currentState!.pushNamed(routeName);
   }
 
